071927783a3fd425db94959378bc8ffdf1cf2581
ShaunApps <shaun.apps@gmail.com>
2016-04-03 23:14:30 -0400

    built out request function

--- server.js
+++ server.js
@@ -7,7 +7,22 @@ var app = express();
 app.get('/scrape', function(req, res){
 
   // web scraping stuff will happen here
+  url = 'http://www.imdb.com/title/tt1229340/';
 
+  // first parameter is the URl \
+  // callback takes 3 params: an error, response, and html
+  request(url, function(error, res, html){
+
+    // first make sure no errors occur
+
+    if(!error){
+      // next we use the cheerio thing on the html
+      var $ = cheerio.load(html);
+
+      var title, release, rating;
+      var json = { title: "", release: "", rating: ""};
+    }
+  })
 })
 
 app.listen('8081')
