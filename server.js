eec7a3aa99d7038ad7d7d542a88cce250ed58d24
ShaunApps <shaun.apps@gmail.com>
2016-04-03 23:07:33 -0400

    setup complete

--- server.js
+++ server.js
@@ -3,3 +3,15 @@ var fs = require('fs');
 var request = require('request');
 var cheerio = require('cheerio');
 var app = express();
+
+app.get('/scrape', function(req, res){
+
+  // web scraping stuff will happen here
+
+})
+
+app.listen('8081')
+
+console.log('scraping happens on port 8081');
+
+exports = module.exports = app;
